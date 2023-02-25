468bcbe7077a3855cf9a01a2e8cd5614e0059029
Control <zhaozihe1105@163.com>
2023-02-25 17:25:10 +0800

    Changes

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="app">
+    <header>
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+      <div class="wrapper">
+        <HelloWorld msg="You did it!" />
+
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+
+      <el-button type="">哈哈哈哈</el-button>
+    </header>
+
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+.app {
+  width: 100vw;
+  height: 100vh;
+  background-color: beige;
+}
+</style>
